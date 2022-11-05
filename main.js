9febc008670fa4538253c798fe9a5bd617546c7c
Amanda Forero <amandaforero@outlook.com>
2022-11-05 13:08:08 -0500

    Se elimina sección de bienvenida, se ajusta a pantalla completa, se añade funcionalidad para modo oscuro

--- main.js
+++ main.js
@@ -2,6 +2,35 @@
 var operandoa;
 var operandob;
 var operacion;
+var changeBotton=false;
+//Modo oscuro
+
+const btnSwitch= document.querySelector('#switch');
+/*const =document.querySelector('');
+const =document.querySelector('');*/
+//
+btnSwitch.addEventListener('click',() => { 
+ // document.body.classList.toggle('dark');//Metodo toggle para colocar una clase al dar click
+  btnSwitch.classList.toggle('active');
+  changeBotton=!changeBotton //Operador not para cambiar el estado de la variable a true
+  if(changeBotton){ 
+    document.documentElement.style.setProperty('--calculadora','#380725')//Accedemos a los estilos y con el metodo .setProperty modificamos las variables que necesitemos en CSS
+    document.documentElement.style.setProperty('--fondo-teclas','#EB7CC3')
+    document.documentElement.style.setProperty('--numeros','#792858')
+    document.documentElement.style.setProperty('--operadores','#9A035D')
+    document.documentElement.style.setProperty('--borde-teclas','#3D1F31')
+    document.documentElement.style.setProperty('--background','linear-gradient(180deg, #000000 0%, #323232 100%)')
+    document.documentElement.style.setProperty('--font-h3','white')
+  }else{
+    document.documentElement.style.setProperty('--calculadora','#D47A7A')
+    document.documentElement.style.setProperty('--fondo-teclas','#E3B7B7')
+    document.documentElement.style.setProperty('--numeros','transparent')
+    document.documentElement.style.setProperty('--operadores','#D99595')
+    document.documentElement.style.setProperty('--borde-teclas','white')
+    document.documentElement.style.setProperty('--background','linear-gradient(1.42deg, #FFFFFF 0.82%, #CDCDCD 98.44%)')
+    document.documentElement.style.setProperty('--font-h3','black')
+  }
+});
 
 function init(){
   //variables
@@ -54,6 +83,7 @@ nueve.onclick = function(e){
 cero.onclick = function(e){
   resultado.textContent = resultado.textContent  + "0";
 }
+
 reset.onclick = function(e){
   resetear();
 }
